c682df30fe3dfb39f3880739c5915ed290294c82
Hrive1007 <152309382+Hrive1007@users.noreply.github.com>
2024-09-27 10:02:12 +0200

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('Buscapisos y Calculapisos')
 
 st.write('Hello world!')
+
+st.info('Esto es una app para buscar pisos en tu ciudad y para entimar el precio de tu piso')
